4012c6b8eefe38ffa4f09b69e7a718de381dd950
HemzaBenhida <benhida.hamza96@gmail.com>
2025-06-22 16:38:21 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,15 @@
+-- Create Database
+CREATE DATABASE DataWarehouse;
+USE DataWarehouse;
+GO
 
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+Create SCHEMA silver; 
+GO 
+
+CREATE SCHEMA gold;
+GO
